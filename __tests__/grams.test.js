70b35f5581ea0fd579f55e7e13873eca327cb16c
Fiona Ochs <fiona.oaks@gmail.com>
2020-03-31 15:43:28 -0700

    gram routes

--- __tests__/grams.test.js
+++ __tests__/grams.test.js
@@ -1,51 +1,51 @@
-// const { getGram, getGrams, getUser } = require('../db/data-helpers');
+const { getGram, getGrams, getUser, getAgent } = require('../db/data-helpers');
 
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-// describe('app routes', () => {
-//   it('creates a gram', () => {
-//     const user = getUser();
-//     return request(app)
-//       .post('/api/v1/grams')
-//       .send({
-//         caption: 'test caption',
-//         photoUrl: 'url',
-//         tags: ['test tag'],
-//         author: user._id
-//       })
-//       .then(res => {
-//         expect(res.body).toEqual({
-//           _id: expect.any(String),
-//           caption:  expect.any(String),
-//           photoUrl:  expect.any(String),
-//           tags: [expect.any(String)],
-//           author: expect.any(String),
-//           __v: 0
-//         });
-//       });
-//   });
+describe('grams routes', () => {
+  it('creates a gram', async() => {
+    const user = await getUser({ username: 'test@test.com' });
+    
+    return getAgent()
+      .post('/api/v1/grams')
+      .send({
+        caption: 'test caption',
+        photoUrl: 'url',
+        tags: ['tag'],
+        author: user._id
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          caption:  expect.any(String),
+          photoUrl:  expect.any(String),
+          tags: [expect.any(String)],
+          author: expect.any(String),
+          __v: 0
+        });
+      });
+  });
 
-//   it('gets a gram by id', async() => {
-//     const gram = await getGram();
-//     const users = await getUsers({ author: gram._id });
+  it('gets a gram by id', async() => {
+    const user = await getUser({ username: 'test@test.com' });
+    const gram = await getGram({ author: user._id });
+    return getAgent()
+      .get(`/api/v1/grams/${gram._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          ...gram,
+          author: user._id
+        });
+      });
+  });
 
-//     return request(app)
-//       .get(`/api/v1/grams/${gram._id}`)
-//       .then(res => {
-//         expect(res.body).toEqual({
-//           ...gram,
-//           films: expect.arrayContaining(films)
-//         });
-//       });
-//   });
-
-//   it('gets all actors', async() => {
-//     const actors = await getActors();
-//     return request(app)
-//       .get('/api/v1/actors')
-//       .then(res => {
-//         expect(res.body).toEqual(actors);
-//       });
-//   });
-// });
+  it('gets all grams', async() => {
+    const grams = await getGrams();
+    return request(app)
+      .get('/api/v1/grams')
+      .then(res => {
+        expect(res.body).toEqual(grams);
+      });
+  });
+});
--- db/seed.js
+++ db/seed.js
@@ -2,22 +2,23 @@ const chance = require('chance').Chance();
 const User = require('../lib/models/User');
 const Gram = require('../lib/models/Gram');
 
-module.exports = async({ usersToCreate = 5, notesToCreate = 100 } = {}) => {
+module.exports = async({ usersToCreate = 5, gramsToCreate = 100 } = {}) => {
   const loggedInUser = await User.create({
     username: 'test@test.com',
     password: 'password',
     profilePhotoUrl: 'url'
   });
 
-  // const users = await User.create([...Array(usersToCreate)].slice(1).map(() => ({
-  //   username: `${chance.profession()} ${chance.animal()}`,
-  //   password: chance.animal(),
-  //   profilePhotoUrl: chance.url()
-  // })));
+  const users = await User.create([...Array(usersToCreate)].slice(1).map(() => ({
+    username: `${chance.profession()} ${chance.animal()}`,
+    password: chance.animal(),
+    profilePhotoUrl: chance.url()
+  })));
 
-  // await Gram.create([...Array(notesToCreate)].map(() => ({
-  //   title: chance.profession(),
-  //   body: chance.sentence(),
-  //   author: chance.weighted([loggedInUser, ...users], [2, ...users.map(() => 1)])._id
-  // })));
+  await Gram.create([...Array(gramsToCreate)].map(() => ({
+    caption: chance.sentence(),
+    photoUrl: chance.url(),
+    tags: [chance.animal()],
+    author: chance.weighted([loggedInUser, ...users], [2, ...users.map(() => 1)])._id
+  })));
 };
