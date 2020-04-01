dcbde6da1f83047280db8e14c8d77f4bb1e7e44e
Fiona Ochs <fiona.oaks@gmail.com>
2020-04-01 13:18:18 -0700

    travis

--- __tests__/grams.test.js
+++ __tests__/grams.test.js
@@ -1,8 +1,5 @@
 const { getGram, getGrams, getUser, getAgent, getComments } = require('../db/data-helpers');
 
-// const request = require('supertest');
-// const app = require('../lib/app');
-
 describe('grams routes', () => {
   it('creates a gram', async() => {
     const user = await getUser({ username: 'test@test.com' });
