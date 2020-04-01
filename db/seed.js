277bc8081073838b29875f1a6db8c1a0a65d6cf3
Fiona Ochs <fiona.oaks@gmail.com>
2020-03-31 17:12:39 -0700

    comment routes passing

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,9 @@
 const chance = require('chance').Chance();
 const User = require('../lib/models/User');
 const Gram = require('../lib/models/Gram');
+const Comment = require('../lib/models/Comment');
 
-module.exports = async({ usersToCreate = 5, gramsToCreate = 100 } = {}) => {
+module.exports = async({ usersToCreate = 5, gramsToCreate = 100, commentsToCreate = 100 } = {}) => {
   const loggedInUser = await User.create({
     username: 'test@test.com',
     password: 'password',
@@ -15,10 +16,17 @@ module.exports = async({ usersToCreate = 5, gramsToCreate = 100 } = {}) => {
     profilePhotoUrl: chance.url()
   })));
 
-  await Gram.create([...Array(gramsToCreate)].map(() => ({
+  const gram = await Gram.create([...Array(gramsToCreate)].map(() => ({
     caption: chance.sentence(),
     photoUrl: chance.url(),
     tags: [chance.animal()],
     author: chance.weighted([loggedInUser, ...users], [2, ...users.map(() => 1)])._id
   })));
+
+  await Comment.create([...Array(commentsToCreate)].map(() => ({
+    comment: chance.sentence(),
+    commentBy: chance.pickone(users),
+    gram: chance.pickone(gram)
+  })));
+
 };
